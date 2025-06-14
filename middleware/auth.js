db43177ce7cf3c68508d8b3c97ba18ad82098bfe
Ayush Kumar <89991492+ayush68824@users.noreply.github.com>
2025-06-14 14:48:19 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
+function authenticateUser(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) return res.status(401).json({ error: 'No token provided' });
   const token = authHeader.split(' ')[1];
@@ -11,6 +11,6 @@ module.exports = (req, res, next) => {
   } catch (err) {
     res.status(401).json({ error: 'Invalid token' });
   }
-};
+}
 
-module.exports = { authenticateUser };
+module.exports = authenticateUser;
