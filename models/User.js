c01e6037a4dcd3ac266cc9763d674adfc1b43c1c
Ayush Kumar <89991492+ayush68824@users.noreply.github.com>
2025-06-14 14:33:40 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,17 +3,23 @@ const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: { type: String },
+  avatar: { type: String },
+  password: { type: String },
 });
 
+// Hash password before saving (only if not Google user)
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
+  if (!this.password) return next(); // Allow Google users with no password
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-userSchema.methods.comparePassword = function (password) {
-  return bcrypt.compare(password, this.password);
+// Password comparison method
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  if (!this.password) return false; // Google users may not have a password
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
